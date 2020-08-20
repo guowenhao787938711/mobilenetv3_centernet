2990be7ac4565e26d23e954747226911655b5d7b
liangzi <liangzi@weibo.staff.com>
2020-08-20 14:39:56 +0800

    change bilinear upsample

--- lib/core/model/head/centernet_head.py
+++ lib/core/model/head/centernet_head.py
@@ -7,6 +7,7 @@ from lib.core.model.net.arg_scope.resnet_args_cope import resnet_arg_scope
 from train_config import config as cfg
 
 from lib.core.model.sqeeze_excitation.se import se
+from lib.core.model.net.mobilenetv3.mobilnet_v3 import hard_swish
 
 class CenternetHead():
 
@@ -21,9 +22,9 @@ class CenternetHead():
                 deconv_feature = self._unet_magic(fms)
 
                 #####
-                kps = slim.conv2d(deconv_feature,
+                kps = slim.separable_conv2d(deconv_feature,
                                   cfg.DATA.num_class,
-                                  [1, 1],
+                                  [3, 3],
                                   stride=1,
                                   activation_fn=None,
                                   normalizer_fn=None,
@@ -31,10 +32,9 @@ class CenternetHead():
                                   biases_initializer=tf.initializers.constant(-2.19),
                                   scope='centernet_cls_output')
 
-
-                wh = slim.conv2d(deconv_feature,
+                wh = slim.separable_conv2d(deconv_feature,
                                  4,
-                                 [1, 1],
+                                 [3, 3],
                                  stride=1,
                                  activation_fn=None,
                                  normalizer_fn=None,
@@ -44,41 +44,53 @@ class CenternetHead():
 
         return kps, wh*16
 
-    def _complex_upsample(self,fm,output_dim, factor=2,scope='upsample'):
-        with tf.variable_scope(scope):
 
 
-            x = slim.separable_conv2d(fm,
-                                       output_dim//2,
-                                       [3, 3],
-                                       activation_fn=None,
-                                       padding='SAME',
-                                       scope='branch_x_upsample_resize')
-            y = slim.separable_conv2d(fm,
-                                       output_dim//2,
-                                       [5, 5],
-                                       activation_fn=None,
-                                       padding='SAME',
-                                       scope='branch_y_upsample_resize')
-            final = x+y
-            final = tf.keras.layers.UpSampling2D(data_format='channels_last', interpolation='bilinear',
-                                                          size=(factor, factor))(final)
-
-            return final
-
-    def _unet_magic(self, fms, dims=cfg.MODEL.head_dims):
-
+    def _unet_magic(self, fms, dim=96):
         c2, c3, c4, c5 = fms
 
-        ####24, 116, 232, 256,
+        ###fm 32
+        c5 = slim.conv2d(c5,
+                         dim,
+                         [1, 1],
+                         activation_fn=None,
+                         padding='SAME',
+                         scope='c5_1x1')
+        c5_upsample=tf.keras.layers.UpSampling2D(size=(2, 2),interpolation='bilinear')(c5)
+        c4 = slim.conv2d(c4,
+                         dim,
+                         [1, 1],
+                         activation_fn=None,
+                         padding='SAME', scope='c4_1x1')
 
-        c5_upsample = self._complex_upsample(c5, output_dim= dims[0],factor=8, scope='c5_upsample')
+        p4 = hard_swish(c4 + c5_upsample)
 
-        c4_upsample = self._complex_upsample(c4, output_dim= dims[1], factor=4,scope='c4_upsample')
+        p4_upsample = tf.keras.layers.UpSampling2D(size=(2, 2),interpolation='bilinear')(p4)
+        p4_upsample = slim.conv2d(p4_upsample,
+                         dim,
+                         [1, 1],
+                         activation_fn=None,
+                         padding='SAME', scope='p4_1x1')
 
-        c3_upsample = self._complex_upsample(c3, output_dim= dims[2],factor=2, scope='c3_upsample')
+        c3 = slim.conv2d(c3,
+                         dim,
+                         [1, 1],
+                         activation_fn=None,
+                         padding='SAME', scope='c3_1x1')
 
-        final = tf.concat([c5_upsample,c4_upsample,c3_upsample,c2],axis=3)
+        p3 = hard_swish(c3 + p4_upsample)
 
-        return final
+        p3_upsample = tf.keras.layers.UpSampling2D(size=(2, 2),interpolation='bilinear')(p3)
+        p3_upsample = slim.conv2d(p3_upsample,
+                                  dim,
+                                  [1, 1],
+                                  activation_fn=None,
+                                  padding='SAME', scope='p3_1x1')
+        c2 = slim.conv2d(c2,
+                         dim,
+                         [1, 1],
+                         activation_fn=None,
+                         padding='SAME', scope='c2_1x1')
+        combine_fm = hard_swish(c2 + p3_upsample)
 
+        return combine_fm
